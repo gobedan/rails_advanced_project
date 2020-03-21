9919f11c3b011df677b73eae28bb7c324d9f27a8
Valentin Markin <jobedano@gmail.com>
2020-03-21 13:32:20 +0400

    4 controllers create only (#2)
    * answer controller and specs implemented
    
    * user can only create questions\answers for now

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class AnswersController < ApplicationController
+  expose(:answer, ancestor: :question)
+  expose(:answers) { Question.find(params[:question_id]).answers }
+  
+  def create
+    answer.question = Question.find(params[:question_id])
+    if answer.save
+      redirect_to question_answer_path(answer.question, answer)
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -28,47 +28,4 @@ RSpec.describe QuestionsController, type: :controller do
       end
     end
   end
-
-  describe 'PATCH #update' do
-    context 'with valid attributes' do
-      it 'changes question attributes' do
-        patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }
-        question.reload
-        expect(question.title).to eq 'new title'
-        expect(question.body).to eq 'new body'
-      end
-
-      it 'redirects to show view' do
-        patch :update, params: { id: question, question: attributes_for(:question) }
-        expect(response).to redirect_to question
-      end
-    end
-
-    context 'with invalid attributes' do
-      before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) } }
-
-      it 'does not save the question' do
-        question.reload
-        expect(question.title).to eq 'MyString'
-        expect(question.body).to eq 'MyText'
-      end
-
-      it 're-renders edit view' do
-        expect(response).to render_template :edit
-      end
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    let!(:question) { create(:question) }
-
-    it 'delete the question' do
-      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
-    end
-
-    it 'redirects to index' do
-      delete :destroy, params: { id: question }
-      expect(response).to redirect_to questions_path
-    end
-  end
 end
