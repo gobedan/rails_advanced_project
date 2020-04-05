fcb912e1b32c7858c586bfc632695b4303c68d1e
Valentin Markin <jobedano@gmail.com>
2020-04-05 18:21:57 +0400

    screencast: devise added

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User can sign in', %q{
+  In ordrer to create or edit content
+  As a guest user 
+  I'd like to be able to sign in
+} do 
+  scenario 'Registered user tries to sign in' do
+    User.create!(email: 'user@test.com', password: '12345678')
+
+    visit '/login'
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+
+    expect(page).to have_content 'Signed in succesfully.'
+
+  end
+
+  scenario 'Unregistered user tries to sign in'
+end
