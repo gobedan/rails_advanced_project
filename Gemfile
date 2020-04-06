3bf6ef68f8419e36afda533f39ba9e199bc9e678
Valentin Markin <jobedano@gmail.com>
2020-04-06 21:18:59 +0400

    sign in spec successful

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.0'
+ruby '2.7.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
@@ -44,7 +44,7 @@ group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'web-console', '>= 3.3.0'
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'launchy'
   gem 'meta_request'
   gem 'rubocop', require: false
   gem 'rubocop-performance', require: false
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -7,15 +7,26 @@ feature 'User can sign in', "
   As a guest user
   I'd like to be able to sign in
 " do
+
+  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+
+  background { visit new_user_session_path }
+
   scenario 'Registered user tries to sign in' do
-    User.create!(email: 'user@test.com', password: '12345678')
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
 
-    visit '/login'
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
-
-    expect(page).to have_content 'Signed in succesfully.'
+    expect(page).to have_content 'Signed in successfully.'
   end
 
-  scenario 'Unregistered user tries to sign in'
+  scenario 'Unregistered user tries to sign in' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password.'
+  end
 end
