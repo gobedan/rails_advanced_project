ba2ef899ff5385ec0c87ba4e3cb4b524e4e2b422
Valentin Markin <jobedano@gmail.com>
2020-05-06 23:11:17 +0400

    Question model has one attached file

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,8 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :author, class_name: "User", inverse_of: :questions
 
+  has_one_attached :file
+
   validates :title, :body, presence: true
 
   def best_answer
