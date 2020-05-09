35528f8f4c5bab79f79a4fd4d2e19be2fce1f785
Valentin Markin <jobedano@gmail.com>
2020-05-09 23:22:30 +0400

    AWS S3 configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,6 +67,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.after(:all) do
+    FileUtils.rm_rf(Rails.root.join("tmp/storage"))
+  end
 end
 
 # GOBEDAN: Configuring shoulda matchers
