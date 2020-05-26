effb542fb2ec5eb73f6bf907674fb0f4bf7e648f
Valentin Markin <jobedano@gmail.com>
2020-05-26 18:46:32 +0400

    url validation, gist embed

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -9,8 +9,24 @@ feature 'User cann add links to question', "
 " do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/gobedan/4651d1193e459cf6c90c34ee93d071ac' }
+  given(:google_url) { 'http://google.ru' }
 
   scenario 'User adds link when asks question' do
+    sign_in(user)
+    visit new_question_path
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text text'
+
+    fill_in 'Link name', with: 'google'
+    fill_in 'Url', with: google_url
+
+    click_on 'Ask'
+
+    expect(page).to have_link 'google', href: google_url
+  end
+
+  scenario 'User adds GitHub Gist link when asks question', js: true do
     sign_in(user)
     visit new_question_path
 
@@ -23,5 +39,6 @@ feature 'User cann add links to question', "
     click_on 'Ask'
 
     expect(page).to have_link 'My gist', href: gist_url
+    expect(page).to have_selector('.gist-data')
   end
 end
