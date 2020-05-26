fac038ae66ec033aa8c3587a201a1af007839b1f
Valentin Markin <jobedano@gmail.com>
2020-05-26 18:46:32 +0400

    url validation, gist embed

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Link < ApplicationRecord
+  URL_REGEX = %r/(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix.freeze
+
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
+  validates :url, format: URL_REGEX
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -6,4 +6,6 @@ RSpec.describe Link, type: :model do
   it { should belong_to :linkable }
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+  it { should allow_value('https://gist.github.com/gobedan/4651d1193e459cf6c90c34ee93d071ac').for(:url) }
+  it { should_not allow_value('htt://gist.github.c/gobedan/4651d1193e459cf6c90c34ee93d071ac').for(:url) }
 end
